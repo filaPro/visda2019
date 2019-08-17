37f2ae1adab13d7e3f48e96a507a72cea5688aac
Danila Rukhovich <danrukh@gmail.com>
2019-08-17 18:47:08 +0300

    start sda_tcl model

---
+++ models/sda_tcl.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+
+
+class SdaTclTrainStep:
+    def __init__(self, build_backbone_lambda, build_bottom_lambda):
+        pass
+
+    @tf.function
+    def train(self, batch):
+        pass
